01acb4222ae87788c796b807e220603470c7455c
Alex Devero <deveroalex@gmail.com>
2021-01-09 10:45:57 +0100

    Create endpoint to get all users

--- server/routes/users-route.js
+++ server/routes/users-route.js
@@ -2,3 +2,18 @@ const express = require('express')
 const User = require('./../models/users-model')
 
 const router = express.Router()
+
+// Get all existing users
+router.get('/', async (req, res) => {
+  try {
+    const allUsers = await User.find()
+    res.json(allUsers)
+  }
+
+  catch(err) {
+    res.status(500).json({
+      message: err.message
+    })
+  }
+})
+
