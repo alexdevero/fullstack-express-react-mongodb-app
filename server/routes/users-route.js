10ca9c3343148aa0d459652fdb3be12e6d7f4f0a
Alex Devero <deveroalex@gmail.com>
2021-01-09 10:48:54 +0100

    Add endpoint to delete existing user

--- server/routes/users-route.js
+++ server/routes/users-route.js
@@ -57,3 +57,21 @@ router.post('/create', async (req, res) => {
   }
 })
 
+// Update existing user
+router.patch('/update/:username', async (req, res) => {})
+
+// Delete existing user
+router.delete('/delete/:id', async (req, res) => {
+  try {
+    const user = await User.remove({ username: req.params.id })
+
+    res.status(200).json({ message: 'User deleted.' })
+  }
+
+  catch (err) {
+    res.status(500).json({
+      message: err.message
+    })
+  }
+})
+
